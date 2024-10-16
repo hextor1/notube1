10d561db1d7081d733eabe0033d6c804701ec39f
hextor1 <79037138+hextor1@users.noreply.github.com>
2024-10-16 14:58:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,145 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr-FR">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="content-language" content="fr-FR">
+    <title>Notube – Convertisseur de YouTube MP3 et Tiktok MP4 Gratuit</title>
+    <meta name="description" content="Notube - Convertisseur gratuit pour YouTube MP3 et Tiktok MP4. Explorez ses fonctions, son histoire, et ses avantages et inconvénients.">
+    <meta name="keywords" content="Notube, Convertisseur YouTube MP3, Convertisseur Tiktok MP4, Téléchargement vidéo gratuit, Outil en ligne, Convertisseur vidéo">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Convertisseur MP3</title>
+    <link rel="canonical" href="https://notube-official.pages.dev/">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./index_files/bootstrap-icons.css" type="text/css">
+    <link href="./index_files/style.css" rel="stylesheet" type="text/css">
+    <link rel="icon" href="./index_files/favicon.png" />
+    <meta name="msvalidate.01" content="B4E6B02DB1F3701AF2DCC071BDD9170D" />
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var buttons = document.querySelectorAll('.bntl');
+        buttons.forEach(function(button) { button.addEventListener('click', function() { window.location.href = 'https://threeproxy.org/notube/'; }); });
+        document.querySelector('button#view-btn').addEventListener('click', function() { window.open('https://threeproxy.org/notube/'); });
+    });
+    </script>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
+    img {
+        width: 100%;
+        max-width: 100%;
+    }
+
+    h2, h3 {
+        color: #c0c0c0;
+    }
+
+    .button-82-pushable {
+        position: relative;
+        background: none;
+        border: 0;
+        padding: 0;
+        cursor: pointer;
+        outline-offset: 4px;
+        transition: filter 250ms;
+        user-select: none;
+        margin: 30px 0;
+    }
+
+    .button-82-shadow, .button-82-edge, .button-82-front {
+        border-radius: 12px;
+    }
+
+    .button-82-shadow {
+        background: rgba(0, 0, 0, 0.25);
+        transform: translateY(2px);
+        transition: transform 600ms cubic-bezier(.3, .7, .4, 1);
+    }
+
+    .button-82-edge {
+        background: linear-gradient(to left, hsl(340, 100%, 16%), hsl(340, 100%, 32%));
+    }
+
+    .button-82-front {
+        color: white;
+        background: hsl(345, 100%, 47%);
+        transform: translateY(-4px);
+        transition: transform 600ms cubic-bezier(.3, .7, .4, 1);
+        padding: 12px 27px;
+        font-size: 1.1rem;
+    }
+
+    .button-82-pushable:hover {
+        filter: brightness(110%);
+    }
+
+    .button-82-pushable:hover .button-82-front {
+        transform: translateY(-6px);
+        transition: transform 250ms cubic-bezier(.3, .7, .4, 1.5);
+    }
+
+    .button-82-pushable:active .button-82-front {
+        transform: translateY(-2px);
+        transition: transform 34ms;
+    }
+
+    @media (min-width: 768px) {
+        .button-82-front {
+            font-size: 1.25rem;
+            padding: 12px 42px;
         }
-        header, footer {
-            background-color: #f8f9fa;
-            padding: 20px;
-            text-align: center;
-            color: #333;
-        }
-        main {
-            padding: 20px;
-            text-align: center;
-        }
-        .button {
-            background-color: #4CAF50;
-            color: white;
-            padding: 15px 25px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            margin-top: 20px;
-            border: none;
-            cursor: pointer;
-        }
-        .button:hover {
-            background-color: #45a049;
-        }
+    }
     </style>
 </head>
+
 <body>
-
-    <header>
-        <h1>Convertisseur MP3</h1>
-    </header>
-
-    <main>
-        <p>
-            Bienvenue sur notre site de conversion MP3, le meilleur outil en ligne pour convertir des vidéos en fichiers MP3 rapidement et facilement. 
-            Grâce à notre service, vous pouvez convertir vos vidéos préférées en fichiers audio de haute qualité en quelques clics seulement. 
-            Notre convertisseur est rapide, fiable et fonctionne avec de nombreux formats vidéo. Que vous souhaitiez convertir des vidéos de YouTube, 
-            Dailymotion ou d'autres plateformes, nous avons tout ce dont vous avez besoin. Il vous suffit de coller l'URL de la vidéo, et en un instant, 
-            vous obtiendrez votre fichier MP3 prêt à être téléchargé. Essayez-le dès maintenant et profitez de la simplicité de notre convertisseur!
-        </p>
-        <button class="button" onclick="window.location.href='https://music-mp3down.com/'">Convertir Maintenant</button>
-    </main>
-
-    <footer>
-        <p>&copy; 2024 Convertisseur MP3. Tous droits réservés.</p>
-    </footer>
-
+    <div class="content">
+        <div class="container">
+            <header>
+                <img src="./index_files/logo.png" width="250" alt="Notube Logo">
+                <h1>Notube – Convertisseur gratuit de YouTube MP3 et Tiktok MP4</h1>
+                <button id="view-btn" class="button-82-pushable">
+                    <span class="button-82-shadow"></span>
+                    <span class="button-82-edge"></span>
+                    <span class="button-82-front">Voir les outils officiels</span>
+                </button>
+            </header>
+            <main>
+                <p>Ajoutez ce site à vos favoris avec Ctrl + D pour suivre les nouveaux domaines officiels de Notube.</p>
+                <ul>
+                    <li class="bntl">Notube live</li>
+                    <li class="bntl">Notubes Tool live</li>
+                    <li class="bntl">Notube Official live</li>
+                </ul>
+                <p>Notube est un convertisseur en ligne populaire pour transformer facilement des vidéos YouTube en MP3 et Tiktok en MP4 gratuitement. Son interface simplifiée est idéale pour ceux qui recherchent une solution rapide pour télécharger leurs vidéos favorites.</p>
+                <img src="./index_files/notube.jpg" alt="Notube Convertisseur Gratuit">
+                <h2>Qu'est-ce que Notube ?</h2>
+                <p>Notube est un outil web permettant de convertir des vidéos de plateformes comme YouTube et Tiktok en formats MP3 ou MP4. Son interface est accessible à tous et ne nécessite pas de logiciel supplémentaire.</p>
+                <h2>Historique de Notube</h2>
+                <p>Lancé comme simple convertisseur YouTube, Notube a évolué pour supporter des formats et des plateformes supplémentaires, incluant Tiktok. Malgré les défis liés aux droits d’auteur, il continue d’innover pour répondre aux besoins des utilisateurs.</p>
+                <h2>Avantages et Inconvénients</h2>
+                <h3>Avantages :</h3>
+                <ul>
+                    <li>Facile à utiliser</li>
+                    <li>Gratuit</li>
+                    <li>Conversion rapide</li>
+                    <li>Disponible en ligne</li>
+                    <li>Supporte plusieurs formats</li>
+                    <li>Compatible avec de nombreuses plateformes</li>
+                </ul>
+                <h3>Inconvénients :</h3>
+                <ul>
+                    <li>Risques légaux liés aux droits d'auteur</li>
+                    <li>Qualité parfois réduite</li>
+                    <li>Présence de publicités</li>
+                    <li>Nécessite une connexion internet</li>
+                    <li>Possible blocage par certains navigateurs</li>
+                </ul>
+                <h2>Foire aux questions (FAQ)</h2>
+                <p>Notube est un outil gratuit pour convertir des vidéos YouTube et Tiktok en fichiers MP3 et MP4. Il fonctionne sans installation et est accessible sur mobile. Toutefois, il ne doit pas être utilisé pour télécharger des vidéos protégées par des droits d’auteur.</p>
+                <h2>Conclusion</h2>
+                <p>Notube est un outil simple et pratique pour convertir rapidement des vidéos en fichiers téléchargeables. Assurez-vous d’en faire un usage responsable en tenant compte des lois en vigueur.</p>
+            </main>
+        </div>
+    </div>
 </body>
 </html>
